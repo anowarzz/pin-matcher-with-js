f1a7abeb9cbb1d8ec43d2135a9453fbdb807e452
anowarzz <anowarhosen444@gmail.com>
2022-08-20 09:29:06 +0600

    pin matcher and verifier added

---
+++ js/app.js
@@ -0,0 +1,64 @@
+function getPin() {
+  const pin = generatePin();
+  const pinString = pin + "";
+  if (pinString.length === 4) {
+    return pin;
+  } else {
+    // console.log("pin not 4 digit found", pin);
+    return getPin();
+  }
+}
+
+function generatePin() {
+  const random = Math.round(Math.random() * 10000);
+  return random;
+}
+
+document.getElementById("generate-pin").addEventListener("click", function () {
+  const pin = getPin();
+  // display pin
+  const displayPinField = document.getElementById("display-pin");
+  displayPinField.value = pin;
+});
+
+document
+  .getElementById("calculator")
+  .addEventListener("click", function (event) {
+    const number = event.target.innerText;
+
+    const typedNumberField = document.getElementById("typed-number");
+    const previousTypedNumber = typedNumberField.value;
+
+    if (isNaN(number)) {
+      if (number === "C") {
+        typedNumberField.value = "";
+      } else if (number === "<") {
+        const digits = previousTypedNumber.split("");
+        digits.pop();
+        const remainingDigits = digits.join("");
+        typedNumberField.value = remainingDigits;
+      }
+    } else {
+      const newTypedNumber = previousTypedNumber + number;
+      typedNumberField.value = newTypedNumber;
+    }
+  });
+
+document.getElementById("verify-pin").addEventListener("click", function () {
+  const displayPinField = document.getElementById("display-pin");
+  const currentPin = displayPinField.value;
+
+  const typedNumberField = document.getElementById("typed-number");
+  const typedNumber = typedNumberField.value;
+
+  const pinSuccessMsg = document.getElementById("pin-success");
+  const pinFailureMsg = document.getElementById("pin-failure");
+
+  if (typedNumber === currentPin) {
+    pinSuccessMsg.style.display = "block";
+    pinFailureMsg.style.display = "none";
+  } else {
+    pinFailureMsg.style.display = "block";
+    pinSuccessMsg.style.display = "none";
+  }
+});
